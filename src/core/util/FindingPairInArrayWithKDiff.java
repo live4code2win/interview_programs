4e5dd652689dd00c1ab4a8af76d4b4b4f1c43eef
live4code2win <mrrat1601@gmail.com>
2020-12-21 00:16:07 +0530

    commit

--- src/core/util/FindingPairInArrayWithKDiff.java
+++ src/core/util/FindingPairInArrayWithKDiff.java
@@ -1,6 +1,5 @@
 package core.util;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -12,56 +11,33 @@ public class FindingPairInArrayWithKDiff {
 		// TODO Auto-generated method stub
 		
 		System.out.println("******************************************* Main Start *******************************************");
-		int  []iArr = new int[]{10,2,3,5,7,2,9,1};
-		System.out.printf("Given Array => [");
-		for (int i : iArr) {
-			System.out.printf("%4d", i);
-		}
-		System.out.printf("  ]");
-		System.out.println();
 		System.out.println("==================================================================================================");
-		System.out.println("Difference constance => 2");
-		Arrays.sort(iArr);
-		System.out.printf("Array => [");
-		for (int i : iArr) {
-			System.out.printf("%4d", i);
-		}
-		System.out.printf("   ]");
-		System.out.println();
-		System.out.println("==================================================================================================");
-		
-		int iLeftMostElement = iArr[0];
-		int iRightMostElement = iArr[iArr.length-1];
-		//List<Integer> al = Arrays.asList(iArr);
 		
 		Integer[] iArr1 = new Integer[] {10,2,3,5,7,2,9,1};
 		List<Integer> list = Arrays.asList(new Integer[] {10,2,3,5,7,2,9,1});
+		System.out.println("Array => "+list);
 		TreeSet<Integer> set = new TreeSet<>(list);
-		System.out.println("Tree set => "+set);
-		//TreeSet<Integer> tsElements = new TreeSet<>(new ArrayList<>(Arrays.asList(iArr)));
+		System.out.println("Tree  => "+set);
+		
 		HashMap<String, Integer> mapResult = new HashMap<>();
-		int iConstant = 2;
-		String strAddedItems = ""; 
+		int iConstant = 1;
 		for (int i = 0; i < iArr1.length; i++) {
 			int iElement = iArr1[i];
-			//int iLeft = 0;
-			//int iRight = 0;
 			int iLeft = iElement - iConstant;
 			int iRight = iElement + iConstant;
+			String leftPair = "";
+			String rightPair = "";
 			if (set.contains(iLeft)) {
-				
+				leftPair = "("+iLeft+","+iElement+")";
+				mapResult.put(leftPair, iElement);
 			}
 			if (set.contains(iRight)) {
-				
+				rightPair = "("+iElement+","+iRight+")";
+				mapResult.put(rightPair, iElement);
 			}
-			if (set.contains(iElement)) {
-				
-			}
-			
 		}
-		
+		System.out.println("Count => "+mapResult.size()+" paires => "+mapResult.keySet());
 		System.out.println("******************************************* Main End *********************************************");
-
 	}
 
 }
