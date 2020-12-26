c9ba418b5e0211a9d087f8a0fec43451ccf93766
live4code2win <mrrat1601@gmail.com>
2020-12-27 01:20:09 +0530

    final check in of merge sort

--- src/core/com/sorting/MergeSort.java
+++ src/core/com/sorting/MergeSort.java
@@ -1,17 +1,68 @@
 package core.com.sorting;
 
+import java.util.Scanner;
+
 public class MergeSort {
 
-	public void merge(int []array, int left_index, int middle_index, int right_index) {
+	public static void merge(int []array, int iLeftIndex, int iMiddleIndex, int iRightIndex) {
+		
+		//TODO :
+		/*
+		 * we will merge the sorted array.
+		 * 
+		 *  left_array => iLeftIndex To iMiddleIndex
+		 *  right_aaray => iMiddleIndex+1 To iRightIndex
+		 */
+		
+		// create two arrays which need to be merged 
+		
+		int _aLeft[] = new int [ (iMiddleIndex - iLeftIndex) +1  ];
+		int _aRight[] = new int [iRightIndex - iMiddleIndex];
+		
+		for (int i = 0; i < _aLeft.length; i++) {
+			_aLeft[i] = array[iLeftIndex+i];			
+		}
+		
+		for (int i = 0; i < _aRight.length; i++) {
+			_aRight[i] = array[iMiddleIndex+i+1];
+		}
+		
+		int i = 0, j = 0, k = iLeftIndex;
+		
+		while (i < _aLeft.length && j < _aRight.length) {
+			if (_aLeft[i] < _aRight[j]) {
+				array[k] = _aLeft[i];
+				i++;
+			}else {
+				array[k] = _aRight[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while (i < _aLeft.length) {
+			array[k] = _aLeft[i];
+			i++;
+			k++;
+		}
+		
+		while (j < _aRight.length) {
+			array[k] = _aRight[j];
+			j++;
+			k++;
+		}
 		
 	}
 	
-	public int [] sort(int [] array, int left_index, int right_index) {
-		
-		int iMiddle_index = left_index+right_index / 2;
-		sort(array, left_index, iMiddle_index);
-		sort(array, iMiddle_index+1, right_index);
-		
+	public static int [] sort(int [] array, int iLeftIndex, int iRightIndex) {
+		//System.out.println("left index => "+iLeftIndex+" right index => "+iRightIndex);
+		if (iLeftIndex < iRightIndex) {
+			int iMiddleIndex = (iLeftIndex+iRightIndex) / 2;
+			sort(array, iLeftIndex, iMiddleIndex);
+			sort(array, iMiddleIndex+1, iRightIndex);
+			merge(array, iLeftIndex, iMiddleIndex, iRightIndex);
+		}
+		//sSystem.out.println("Sort Recurrsion finished...");
 		return array;
 	}
 	
@@ -19,8 +70,34 @@ public class MergeSort {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("================= Main ========================");
-		System.out.println("================= main ends here ========================");
+		System.out.println("====================== Main Merge Sort Algorithm =============================");
+		//int [] inputArray = {22,7,91,5,3,19,60,41,56,8};
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter no. of elements you want in array:");
+        int iElements = sc.nextInt();
+        int inputArray[] = new int[iElements];
+        System.out.printf("Enter all the elements:=>");
+        for(int i = 0; i < iElements; i++)
+        {
+        	inputArray[i] = sc.nextInt();
+        }
+		System.out.println("==============================================================================");
+		System.out.printf("Input Array => [");
+		//int [] inputArray = {2,7,1,5,3,9,0,4,6,8};
+		for (int i = 0; i < inputArray.length; i++) {
+			System.out.printf("%4d", inputArray[i]);
+		}
+		System.out.printf("]");
+		System.out.println();
+		System.out.println("==============================================================================");
+		System.out.printf("Sorted Array => [");
+		int [] resultArray =  sort(inputArray, 0, inputArray.length-1);
+		for (int i = 0; i < resultArray.length; i++) {
+			System.out.printf("%4d", resultArray[i]);
+		}
+		System.out.printf("]");
+		System.out.println();
+		System.out.println("========================== main ends here ====================================");
 		
 	}
 
