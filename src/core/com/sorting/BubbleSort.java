33b8c49b5456984a3f218c5a35619f4e355f4e14
live4code2win <mrrat1601@gmail.com>
2020-12-27 23:54:48 +0530

    added new sort -> bubble sort

---
+++ src/core/com/sorting/BubbleSort.java
@@ -0,0 +1,54 @@
+package core.com.sorting;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+	
+	public static int[] bubbleSort(int [] array) {
+		//System.out.println("Inside the bubble sort method.");
+		for(int i = 0; i < array.length;i++) {
+			for(int j = 0; j < array.length - i -1 ;j++) {
+				if (array[j] > array[j+1]) {
+					int iTempItem = array[j];
+					array[j] = array[j+1];
+					array[j+1] = iTempItem;
+				}
+			}
+		}
+		return array;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("===================== Main Bubble Sort Algorithm =============================");
+		//int [] inputArray = {22,7,91,5,3,19,60,41,56,8};
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter no. of elements you want in array:");
+        int iElements = sc.nextInt();
+        int inputArray[] = new int[iElements];
+        System.out.printf("Enter all the elements:=>");
+        for(int i = 0; i < iElements; i++)
+        {
+        	inputArray[i] = sc.nextInt();
+        }
+		System.out.println("==============================================================================");
+		System.out.printf("Input Array => [");
+		//int [] inputArray = {2,7,1,5,3,9,0,4,6,8};
+		for (int i = 0; i < inputArray.length; i++) {
+			System.out.printf("%4d", inputArray[i]);
+		}
+		System.out.printf("]");
+		System.out.println();
+		System.out.println("==============================================================================");
+		System.out.printf("Sorted Array => [");
+		int [] resultArray =  bubbleSort(inputArray);
+		for (int i = 0; i < resultArray.length; i++) {
+			System.out.printf("%4d", resultArray[i]);
+		}
+		System.out.printf("]");
+		System.out.println();
+		System.out.println("========================== main ends here ====================================");
+
+	}
+
+}
--- src/core/com/sorting/package-info.java
+++ src/core/com/sorting/package-info.java
@@ -3,4 +3,7 @@ package core.com.sorting;
 /*
  * i. MergeSort => 
  * Quickest sorting algorithm , used divide and conquer method/mechanism to do the sorting. 
+ * 
+ * ii. BubbleSort => 
+ * Bubble sort simplest sort, which is achieved by swapping adjacent element repetitively
  */
